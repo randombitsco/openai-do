542da4fe8bcd3f7495cb71aff97e3856e0e63413
David Peterson <david@randombits.org>
2022-09-16 19:46:49 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# swift-openai-cli
+
+A description of this package.
---
+++ Sources/swift-openai-cli/swift_openai_cli.swift
@@ -0,0 +1,6 @@
+public struct swift_openai_cli {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
