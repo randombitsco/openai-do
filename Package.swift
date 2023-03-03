c14c495805f8b21936f17e72374912da93b50f69
David Peterson <david@randombits.org>
2023-03-03 16:40:13 +1000

    Updated Swift Argument Parser version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
 //        .package(url: "https://github.com/randombitsco/swift-openai-bits", branch: "main"),
         .package(url: "../swift-openai-bits", branch: "main"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.5.0"),
         .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.10.0"),
         .package(url: "https://github.com/jordanbaird/Prism", from: "0.0.6"),
